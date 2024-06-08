73bbfe10ee3e3e7fc0f8a2362a2551c7ec69842e
SomeGuyEight <d.howden@outlook.com>
2024-06-07 18:50:57 -0700

    fix name formating

--- src/main/java/com/turtlehoarder/cobblemonchallenge/command/ChallengeCommand.java
+++ src/main/java/com/turtlehoarder/cobblemonchallenge/command/ChallengeCommand.java
@@ -333,10 +333,9 @@ public class ChallengeCommand {
 
 
             String levelComponent = (minLevel == maxLevel) ? ChatFormatting.YELLOW + String.format("You have been challenged to a " + ChatFormatting.BOLD + "level %d Pokemon battle", maxLevel) : ChatFormatting.YELLOW + String.format("You have been challenged to a " + ChatFormatting.BOLD + "level %d - %d Pokemon battle", minLevel, maxLevel);
-            String optionsComponent = request.preview() ? "" : ChatFormatting.GOLD + " [NoTeamPreview]";
             String challengerComponent = ChatFormatting.YELLOW + " by %s!" + challengerPlayer.getDisplayName().getString();
-            String challengedComponent = ChatFormatting.YELLOW + " by %s!" + challengedPlayer.getDisplayName().getString();
-            String handicapComponent = (handicapP1 == 0 && handicapP2 == 0) ? "" : ChatFormatting.YELLOW + "[ " + challengerComponent + " handicap of " + handicapP1 + " ][ " + challengedComponent + " handicap of " + handicapP2 + " ]";
+            String optionsComponent = request.preview() ? "" : ChatFormatting.GOLD + " [NoTeamPreview]";
+            String handicapComponent = (handicapP1 == 0 && handicapP2 == 0) ? "" : ChatFormatting.YELLOW + "[ " + challengerPlayer.getDisplayName().getString() + " handicap of " + handicapP1 + " ][ " + challengedPlayer.getDisplayName().getString() + " handicap of " + handicapP2 + " ]";
             MutableComponent notificationComponent = Component.literal(levelComponent + challengerComponent + optionsComponent + handicapComponent);
 
             MutableComponent interactiveComponent = Component.literal("Click to accept or deny: ");
