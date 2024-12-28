43ae0d0b65495ad1ad4086c63a91cc532a6037ef
Eric G <140836859+TurtleHoarder@users.noreply.github.com>
2024-12-28 00:41:21 -0500

    Starting NeoForge stuff

---
+++ neoforge/src/main/java/com/cobblemonchallenge/forge/CobblemonChallengeForge.java
@@ -0,0 +1,68 @@
+package com.cobblemonchallenge.forge;
+
+import com.cobblemonchallenge.forge.config.ChallengeConfig;
+import com.turtlehoarder.cobblemonchallenge.common.CobblemonChallenge;
+import com.turtlehoarder.cobblemonchallenge.common.CobblemonChallengeCommonInterface;
+import com.turtlehoarder.cobblemonchallenge.common.command.ChallengeCommand;
+import com.turtlehoarder.cobblemonchallenge.common.event.ChallengeEventHandler;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.function.Supplier;
+
+@Mod(CobblemonChallenge.MODID)
+public class CobblemonChallengeForge extends CobblemonChallengeCommonInterface {
+    private static ChallengeConfig config;
+    private static ForgeConfigSpec commonSpec;
+
+    static {
+        final Pair<ChallengeConfig, ForgeConfigSpec> common = new ForgeConfigSpec.Builder().configure(ChallengeConfig::new);
+        config = common.getLeft();
+        commonSpec = common.getRight();
+    }
+
+    public CobblemonChallengeForge() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, commonSpec);
+        MinecraftForge.EVENT_BUS.addListener(this::commands);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::serverInitialize);
+    }
+
+    public void serverInitialize(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            CobblemonChallenge challenge = new CobblemonChallenge();
+            challenge.initializeChallenge(this);
+        });
+    }
+
+    public void commands(RegisterCommandsEvent e) {
+        ChallengeCommand.register(e.getDispatcher());
+    }
+
+    @Override
+    public int getIntConfig(String configName) {
+        return config.getIntConfig(configName);
+    }
+
+    @Override
+    public boolean getBooleanConfig(String configName) {
+        return config.getBooleanConfig(configName);
+    }
+
+    @Override
+    public void registerEvents() {
+        ChallengeEventHandler.registerEvents();
+        //DistExecutor.safeCallWhenOn(Dist.DEDICATED_SERVER, () -> ChallengeEventHandler::registerEvents);
+    }
+
+    @Override
+    public void registerCommands() {}
+}
